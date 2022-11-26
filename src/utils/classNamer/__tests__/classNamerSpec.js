9a2894f776be28e1ecb68ea8af9e611d1601ef39
Jun, Jakub <jakub.jun@firma.seznam.cz>
2022-11-26 23:39:16 +0100

    Add classNamer tests

--- src/utils/classNamer/__tests__/classNamerSpec.js
+++ src/utils/classNamer/__tests__/classNamerSpec.js
@@ -10,8 +10,12 @@ describe('classNamer', () => {
       expect(classNamer('test')).toBe('test');
     });
 
-    test('with multiple string', () => {
+    test('with multiple words in a string', () => {
       expect(classNamer('test color-red padding-right')).toBe('test color-red padding-right');
+    });
+
+    test('with multiple duplicate words in a string', () => {
+      expect(classNamer('test test color-red padding-right')).toBe('test color-red padding-right');
     });
 
     test('with empty object', () => {
@@ -26,6 +30,20 @@ describe('classNamer', () => {
     test('with complex object', () => {
       expect(classNamer({ 'color-red': true, 'color-blue': 1 === 2 })).toBe('color-red');
       expect(classNamer({ 'color-red': false, 'color-blue': 1 === 2 })).toBe('');
+    });
+
+    test('with combination of objects, strings and deduplications', () => {
+      expect(classNamer('test', { test: false })).toBe('test');
+      expect(classNamer(
+        'test color-red',
+        { 'text-xl': true },
+        'color-red',
+        { 'px-3': false },
+      )).toBe('test color-red text-xl');
+    });
+
+    test('and deduplicate words in words from object keys', () => {
+      expect(classNamer({ test: true }, { test: true })).toBe('test');
     });
   });
 
--- src/utils/classNamer/classNamer.js
+++ src/utils/classNamer/classNamer.js
@@ -1,24 +1,43 @@
+const ERROR_STR = 'classNamer takes only strings and objects as arguments';
+
+function reduceStringToClassNameSet(classNameString, classNameSet) {
+  classNameString.split(' ').forEach((word) => classNameSet.add(word));
+}
+
+function reduceObjectToClassNameSet(classNameObject, classNameSet) {
+  Object.entries(classNameObject).forEach(([className, condition]) => {
+    if (condition) {
+      reduceStringToClassNameSet(className, classNameSet);
+    }
+  });
+}
+
 function argumentReducer(classNameSet, argument) {
   if (Array.isArray(argument)) {
-    throw Error('classNamer requires only strings or objects as arguments');
+    throw Error(ERROR_STR);
   }
 
   if (typeof argument === 'string') {
-    return classNameSet.add(argument);
-  }
-
-  if (typeof argument === 'object') {
-    Object.entries(argument).forEach(([className, condition]) => {
-      if (condition) {
-        classNameSet.add(className);
-      }
-    });
+    reduceStringToClassNameSet(argument, classNameSet);
     return classNameSet;
   }
 
-  throw Error('classNamer requires only strings or objects as arguments');
+  if (typeof argument === 'object') {
+    reduceObjectToClassNameSet(argument, classNameSet);
+    return classNameSet;
+  }
+
+  throw Error(ERROR_STR);
 }
 
+/**
+  * To be used for dynamically constructing className string.
+  * an object is given.
+  * @param {...string|object} args
+  * @returns {string} Returns deduplicated word string if a string is given
+  * or deduplicated word string of truthy fields\' keys if an
+  * object is given.
+  */
 export default function classNamer(...args) {
   const classNameSet = args.reduce(argumentReducer, new Set());
   return Array.from(classNameSet).join(' ');
