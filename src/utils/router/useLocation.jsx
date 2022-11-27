4772e00e38ed73f7a58198e242d7479e1c8ccbc0
Jun, Jakub <jakub.jun@firma.seznam.cz>
2022-11-27 11:27:39 +0100

    Remove jsx hooks

--- src/utils/router/useLocation.jsx
+++
@@ -1,3 +0,0 @@
-export default function useLocation() {
-  return window.location.pathname;
-}
--- src/utils/router/useRouter.jsx
+++
@@ -1,8 +0,0 @@
-import useLocation from './useLocation';
-import { routes, notFoundPathName } from '../../config/routes';
-
-export default function useRouter() {
-  const path = useLocation();
-
-  return routes[path] || routes[notFoundPathName];
-}
